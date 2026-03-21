87702d3c8a8e1edc2d66e6247d004b46d45a9ddf
Anderson <andersonamalta@gmail.com>
2026-03-21 07:24:11 -0300

    docs: add jsdocs to user service

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,16 @@
 import { protectedApi, publicApi } from '@/lib/axios'
 
 export const UserService = {
+  /**
+   * Cria um novo usuário.
+   * @param {object} input
+   * @param {string} input.firstName
+   * @param {string} input.lastName
+   * @param {string} input.email
+   * @param {string} input.password
+   * @returns {Object} Usuário criado
+   * @returns {string} response.tokens - Tokens de autenticação
+   */
   signup: async (input) => {
     const response = await publicApi.post('/users', {
       first_name: input.firstName,
@@ -16,6 +26,14 @@ export const UserService = {
       tokens: response.data.tokens,
     }
   },
+  /**
+   * Faz o login do usuário.
+   * @param {object} input
+   * @param {string} input.email
+   * @param {string} input.password
+   * @returns {Object} Usuário autenticado
+   * @returns {string} response.tokens - Tokens de autenticação
+   */
   login: async (input) => {
     const response = await publicApi.post('/users/login', {
       email: input.email,
@@ -29,6 +47,10 @@ export const UserService = {
       tokens: response.data.tokens,
     }
   },
+  /**
+   * Retorna o usuário autenticado
+   * @returns {Object} Usuário autenticado
+   */
   me: async () => {
     const response = await protectedApi.get('/users/me')
 
