01faba229a4e36ed5b77b773cf98ba2a12a40728
Anderson <andersonamalta@gmail.com>
2026-03-21 07:09:31 -0300

    refactor: ensure user properties are returned from service on camelCase

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
   }
   return (
     <>
-      <h1>Olá, {user.first_name}</h1>
+      <h1>Olá, {user.firstName}</h1>
       <Button onClick={signout}>Sair</Button>
     </>
   )
