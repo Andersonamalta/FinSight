2112855bdc32fc5b9a8da3a643c97517b6506bf7
Anderson <andersonamalta@gmail.com>
2026-08-04 18:27:16 -0300

    fix: ensure from and to query params are only considered they are valid

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,5 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query'
-import { addMonths, format } from 'date-fns'
+import { addMonths, format, isValid } from 'date-fns'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
 
@@ -9,20 +9,36 @@ import { DatePickerWithRange } from './ui/date-picker-with-range'
 
 const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd')
 
+const getInitialDateState = (searchParams) => {
+  const defaultDate = {
+    from: new Date(),
+    to: addMonths(new Date(), 1),
+  }
+  if (!searchParams) return defaultDate
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+  if (!from || !to) {
+    return defaultDate
+  }
+  // Neste ponto, eu tenho os dois valores de data, mas preciso validar se eles são datas válidas
+  const datesAreInvalid = !isValid(new Date(from)) || !isValid(new Date(to))
+  // Se forem inválidas, eu retorno a data padrão
+  if (datesAreInvalid) {
+    return defaultDate
+  }
+  // Neste ponto, eu tenho certeza que as datas são válidas, então posso retornar o objeto com as datas convertidas para Date
+  return {
+    from: new Date(from + 'T00:00:00'),
+    to: new Date(to + 'T00:00:00'),
+  }
+}
+
 const DateSelection = () => {
   const queryClient = useQueryClient()
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const { user } = useAuthContext()
-  const [date, setDate] = useState({
-    // 2. Quando eu recarrego a página, eu pego o from e o to da url e persisto eles no state
-    from: searchParams.get('from')
-      ? new Date(searchParams.get('from') + 'T00:00:00')
-      : new Date(),
-    to: searchParams.get('to')
-      ? new Date(searchParams.get('to') + 'T00:00:00')
-      : addMonths(new Date(), 1),
-  })
+  const [date, setDate] = useState(getInitialDateState(searchParams))
   // 1. Sempre que o state "date" mudar, eu preciso persisti-lo na url (?from&to=)
   useEffect(() => {
     if (!date?.from || !date?.to) return
